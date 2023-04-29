618859b1f27e13f21b6afb4152a25c37a380e6b5
Grabtot <dneshotkin@gmail.com>
2023-04-29 14:22:12 +0300

    Кузов может проверять свою наполненность #4

--- Assets/Scripts/Truck/Truck.cs
+++ Assets/Scripts/Truck/Truck.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
 
-public class Truck : MonoBehaviour
+namespace LudumDare53.Truck
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Truck : MonoBehaviour
     {
+        [SerializeField] private Collider2D _cargoCollider;
 
-    }
+        private bool _isFull = false;
+        private float _occupiedArea = 0f;
+        private float _maxArea;
 
-    // Update is called once per frame
-    void Update()
-    {
+        void Start()
+        {
+            _maxArea = _cargoCollider.bounds.size.x * _cargoCollider.bounds.size.y;
+        }
 
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Box"))
+            {
+                float boxArea = other.bounds.size.x * other.bounds.size.y;
+
+                _occupiedArea += boxArea;
+                _isFull = _occupiedArea >= _maxArea - 1;
+
+                if (_isFull)
+                {
+                    Debug.Log("Cargo is full");
+                }
+                Debug.Log("Occupied area: " + _occupiedArea);
+            }
+        }
     }
 }
