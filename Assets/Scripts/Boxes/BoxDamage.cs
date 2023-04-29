ad370647def5ad4ac1f1f906a8e4b0186372ed23
ArtCrafter2999 <ArtCrafter2999@gmail.com>
2023-04-29 14:06:47 +0300

    Готово#2 #5

---
+++ Assets/Scripts/Boxes/BoxDamage.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace LudumDare53.Boxes
+{
+    public class BoxDamage : MonoBehaviour
+    {
+        [SerializeField]
+        public float maxHealth;
+        [SerializeField]
+        private float fragilityLevel;
+        public UnityEvent<float> onDamage;
+    
+        private float _health;
+        public float InterpolatedHealth => _health / maxHealth;
+
+        private void Start()
+        {
+            _health = maxHealth;
+        }
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.relativeVelocity.magnitude >= fragilityLevel)
+            {
+                _health = Mathf.Clamp(_health - col.relativeVelocity.magnitude, 0, maxHealth);
+                onDamage.Invoke(InterpolatedHealth);
+            } 
+        }
+    }
+}
