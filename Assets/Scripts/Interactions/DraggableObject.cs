6bc4db310715d6a73ae498348866a3ee1dda8fb3
DanyloSih <geratosfen@gmail.com>
2023-04-30 01:08:03 +0300

    Release version of physics daragger!

--- Assets/Scripts/Interactions/DraggableObject.cs
+++ Assets/Scripts/Interactions/DraggableObject.cs
@@ -2,20 +2,16 @@
 
 namespace LudumDare53.Interactions
 {
-    [RequireComponent(typeof(Rigidbody2D), typeof(SpringJoint2D))]
+    [RequireComponent(typeof(Rigidbody2D))]
     public class DraggableObject : MonoBehaviour
     {
         private Rigidbody2D _rigidbody2D;
-        private SpringJoint2D _springJoint2D;
 
         public Rigidbody2D Rigidbody2D { get => _rigidbody2D; }
-        public SpringJoint2D SpringJoint2D { get => _springJoint2D; }
 
         protected void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
-            _springJoint2D = GetComponent<SpringJoint2D>();
-            _springJoint2D.enabled = false;
         }
     }
 }
