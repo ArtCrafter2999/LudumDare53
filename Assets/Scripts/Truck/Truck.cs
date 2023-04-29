11c7276098fd073af2dbea05ca39d2cafb1a3f0e
Grabtot <dneshotkin@gmail.com>
2023-04-29 14:57:38 +0300

    Грузовик может въезжать и выезжать из пределов экрана #4

--- Assets/Scripts/Truck/Truck.cs
+++ Assets/Scripts/Truck/Truck.cs
@@ -1,3 +1,5 @@
+using DG.Tweening;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace LudumDare53.Truck
@@ -5,14 +7,36 @@ namespace LudumDare53.Truck
     public class Truck : MonoBehaviour
     {
         [SerializeField] private Collider2D _cargoCollider;
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] protected float _moveDistance = 50f;
+        [SerializeField] protected float _moveDuration = 5f;
 
         private bool _isFull = false;
         private float _occupiedArea = 0f;
         private float _maxArea;
+        private List<GameObject> _boxes;
 
-        void Start()
+        private void Start()
         {
             _maxArea = _cargoCollider.bounds.size.x * _cargoCollider.bounds.size.y;
+            _boxes = new List<GameObject>();
+            MoveIn();
+        }
+
+        public void MoveOut()
+        {
+            DissableBoxesRigidbody();
+            Move(transform.position.x - _moveDistance);
+        }
+
+        public void MoveIn()
+        {
+            Move(transform.position.x + _moveDistance);
+        }
+
+        private void Move(float distance)
+        {
+            transform.DOMoveX(distance, _moveDuration);
         }
 
         private void OnTriggerEnter2D(Collider2D other)
@@ -20,7 +44,7 @@ namespace LudumDare53.Truck
             if (other.CompareTag("Box"))
             {
                 float boxArea = other.bounds.size.x * other.bounds.size.y;
-
+                _boxes.Add(other.gameObject);
                 _occupiedArea += boxArea;
                 _isFull = _occupiedArea >= _maxArea - 1;
 
@@ -28,7 +52,15 @@ namespace LudumDare53.Truck
                 {
                     Debug.Log("Cargo is full");
                 }
-                Debug.Log("Occupied area: " + _occupiedArea);
+            }
+        }
+
+        private void DissableBoxesRigidbody()
+        {
+            foreach (GameObject box in _boxes)
+            {
+                Destroy(box.GetComponent<Rigidbody2D>());
+                box.transform.SetParent(transform);
             }
         }
     }
