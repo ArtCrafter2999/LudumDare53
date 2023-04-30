f6413d887aee65c908a38397f0b1db46e9f2d41c
ArtCrafter2999 <ArtCrafter2999@gmail.com>
2023-04-30 20:04:24 +0300

    Трошки з помиранням коробок і шредер#44 #47 #51 #7 #43

---
+++ Assets/Scripts/Boxes/Shredder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace LudumDare53.Boxes
+{
+    public class Shredder : MonoBehaviour
+    {
+        [SerializeField] private float power;
+        [SerializeField] private float damage;
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (!collision.gameObject.TryGetComponent(out BoxDamage boxDamage)) return;
+            collision.rigidbody.AddForce(Vector2.up*power, ForceMode2D.Impulse);
+            boxDamage.Damage(damage);
+        }
+    }
+}
