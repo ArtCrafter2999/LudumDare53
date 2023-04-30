39343f7d1ca80bc80b8c4705fa0e2e30927c226b
DanyloSih <geratosfen@gmail.com>
2023-04-30 10:19:20 +0300

    Created first prototype scene. closed #32 closed #29

--- Assets/Scripts/Interactions/DraggableObject.cs
+++ Assets/Scripts/Interactions/DraggableObject.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace LudumDare53.Interactions
 {
     [RequireComponent(typeof(Rigidbody2D))]
     public class DraggableObject : MonoBehaviour
     {
+        public UnityEvent ObjectDraggingStarted;
+
         private Rigidbody2D _rigidbody2D;
 
         public Rigidbody2D Rigidbody2D { get => _rigidbody2D; }
+
+        public void StartDragging()
+        {
+            ObjectDraggingStarted?.Invoke();
+        }
 
         protected void Start()
         {
