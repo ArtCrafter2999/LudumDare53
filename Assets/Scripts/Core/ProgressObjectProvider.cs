1fabd2609c84f8208484d50006bd667a5291219d
DanyloSih <geratosfen@gmail.com>
2023-04-30 11:40:52 +0300

    scene edited

---
+++ Assets/Scripts/Core/ProgressObjectProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LudumDare53.Core
+{
+
+    [Serializable]
+    public class ProgressObjectProvider<TValue> 
+        where TValue : class
+    {
+        [SerializeField] private List<ProgressObject<TValue>> _progressObjects = new();
+
+        //public TValue GetObjectByProgress(float progress)
+        //{
+        //    _progressObjects.Sort((a, b) => a.ProgresMark)
+        //}
+    }
+}
