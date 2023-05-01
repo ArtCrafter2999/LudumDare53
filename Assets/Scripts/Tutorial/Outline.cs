e4b4a8547f01b59acd49de45ef7b3e252942a06b
DanyloSih <geratosfen@gmail.com>
2023-05-01 11:00:52 +0300

    Added stroke for boxes, added markers (red , green, blue) for boxes.

---
+++ Assets/Scripts/Tutorial/Outline.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace LudumDare53.Tutorial
+{
+    public class Outline : MonoBehaviour
+    {
+        [SerializeField] private Sprite _outlineSprite;
+        [SerializeField] private SpriteRenderer _outlineSpriteRenderer;
+
+        private Sprite _startSprite;
+
+        public void Activate()
+        {
+            _outlineSpriteRenderer.sprite = _outlineSprite;
+        }
+
+        public void Deactivate()
+        {
+            _outlineSpriteRenderer.sprite = _startSprite;
+        }
+
+        protected void Start ()
+        {
+            _startSprite = _outlineSpriteRenderer.sprite;
+        }
+    }
+}
